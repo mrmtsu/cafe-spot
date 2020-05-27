f3ea6cfdc85994214e9e73e55626181b336a59e9
mrmtsu <81202muramatsu@gmail.com>
2020-05-27 15:23:30 +0900

    お気に入り機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
                                    foreign_key: "followed_id",
                                    dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :favorites, dependent: :destroy
   before_save :downcase_email
   attr_accessor :remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -69,6 +70,18 @@ class User < ApplicationRecord
                      WHERE follower_id = :user_id"
     Post.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
+  end
+
+  def favorite(post)
+    Favorite.create!(user_id: id, post_id: post.id)
+  end
+
+  def unfavorite(post)
+    Favorite.find_by(user_id: id, post_id: post.id).destroy
+  end
+
+  def favorite?(post)
+    !Favorite.find_by(user_id: id, post_id: post.id).nil?
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   post   :login,     to: 'sessions#create'
   delete :logout,    to: 'sessions#destroy'
   resources :relationships, only: [:create, :destroy]
+  get :favorites, to: 'favorites#index'
+  post   "favorites/:post_id/create"  => "favorites#create"
+  delete "favorites/:post_id/destroy" => "favorites#destroy"
 end
